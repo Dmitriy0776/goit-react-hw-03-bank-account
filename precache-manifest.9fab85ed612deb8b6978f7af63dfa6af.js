a8910b8a1e519ef6b59c909851ce1990173ef1d2
Dmitriy <velkov55@gmail.com>
2019-09-10 21:18:05 +0300

    Updates

---
+++ precache-manifest.9fab85ed612deb8b6978f7af63dfa6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8fdb172647b89cfeb419ea9461b120d",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "4f30bf6cd1d1439fcb75",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "8e04d4778adc9f8f6ffa",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.10677856.chunk.css"
+  },
+  {
+    "revision": "4f30bf6cd1d1439fcb75",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.63742add.chunk.js"
+  },
+  {
+    "revision": "8e04d4778adc9f8f6ffa",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.8be94219.chunk.js"
+  },
+  {
+    "revision": "08676760f97f1f538643",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.14ff5211.js"
+  }
+]);
